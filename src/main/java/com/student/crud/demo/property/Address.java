98d0756ffe7580af615700b56b4259aec40307b6
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-03-15 03:06:26 -0300

    create student properties model

---
+++ src/main/java/com/student/crud/demo/property/Address.java
@@ -0,0 +1,30 @@
+package com.student.crud.demo.property;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+  private String street;
+  private String city;
+
+  public Address(final String street, final String city) {
+    this.street = street;
+    this.city = city;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+}
