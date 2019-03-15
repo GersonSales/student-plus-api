d86e6d754972b6e1591cf2c5b31ccd39332ea29a
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-03-15 03:01:03 -0300

    create initial student DTO

---
+++ src/main/java/com/student/crud/demo/student/StudentDTO.java
@@ -0,0 +1,76 @@
+package com.student.crud.demo.student;
+
+import com.student.crud.demo.contract.Convertible;
+import com.student.crud.demo.propertie.Address;
+import com.student.crud.demo.propertie.Name;
+
+public class StudentDTO implements Convertible<StudentModel> {
+
+  private String id;
+  private Long registrationNumber;
+
+  private String firstName;
+  private String lastName;
+
+  private String street;
+  private String city;
+
+  public StudentDTO() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public Long getRegistrationNumber() {
+    return registrationNumber;
+  }
+
+  public void setRegistrationNumber(Long registrationNumber) {
+    this.registrationNumber = registrationNumber;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  @Override
+  public StudentModel convert() {
+    final Name name = new Name(getFirstName(), getLastName());
+    final Address address = new Address(getStreet(), getCity());
+
+    return new StudentModel(getRegistrationNumber(), name, address);
+  }
+}
