6e171d0bb1ad3e0d06fe137a72d83129f899cf5a
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2019-03-17 23:25:03 -0300

    allowing cors origins

--- src/main/java/com/student/crud/demo/StudentCrudApplication.java
+++ src/main/java/com/student/crud/demo/StudentCrudApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -23,4 +26,13 @@ public class StudentCrudApplication {
     SpringApplication.run(StudentCrudApplication.class, args);
   }
 
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("**");
+      }
+    };
+  }
 }
